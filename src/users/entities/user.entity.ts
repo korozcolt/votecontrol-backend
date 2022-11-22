53dd3eda189ab5f9b1a7e32e704421e24d030b6c
Kristian Orozco <korozco@lean-tech.io>
2022-11-22 16:38:07 -0500

    user model

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,51 @@
-export class User {}
+import {
+  Column,
+  Default,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+
+import { UUIDV4 } from 'sequelize';
+
+@Table({ timestamps: true })
+export class User extends Model {
+  @PrimaryKey
+  @Default(UUIDV4)
+  @Column
+  id: string;
+
+  @Column
+  firstName: string;
+
+  @Column
+  lastName: string;
+
+  @Column
+  username: string;
+
+  @Column
+  password: string;
+
+  @Column
+  email: string;
+
+  @Column
+  phone: string;
+
+  @Column
+  address: string;
+
+  @Column
+  city: string;
+
+  @Column
+  state: string;
+
+  @Column
+  country: string;
+
+  @Default(true)
+  @Column
+  isActive: boolean;
+}
